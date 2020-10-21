c7c23e49d87ed294e05605711ec5982a21b92223
Jennifer Henry <jenhenry1995@gmail.com>
2020-10-21 16:03:01 -0700

    set up connection/orm

---
+++ config/orm.js
@@ -0,0 +1,16 @@
+var connection = require('./connection.js')
+
+var orm ={
+
+selectAll: function(){
+
+},
+insertOne: function(){
+
+},
+updateOne: function(){
+
+}
+};
+
+module.exports = orm;
--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,6 @@ USE burgers_db;
 CREATE TABLE burgers(
     id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN NOT NULL,
-    PRIMARY KEY(id)
+    devoured BOOLEAN,
+    PRIMARY KEY (id)
 );
