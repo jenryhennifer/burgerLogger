2cad48a5e864ac0e62bcb957a364741aee94fbbf
Jennifer Henry <jenhenry1995@gmail.com>
2020-10-21 11:49:10 -0700

    created folders/files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Cheese Burger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Veggie Burger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Double Burger', false);
