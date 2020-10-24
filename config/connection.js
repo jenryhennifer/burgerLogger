32b011328ffcb808cfec5527c7a5283786525d68
Jennifer Henry <jenhenry1995@gmail.com>
2020-10-23 18:56:28 -0700

    test

--- config/connection.js
+++ config/connection.js
@@ -4,19 +4,20 @@ if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 }else{
   connection = mysql.createConnection({
-    host: "kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
-    user: "mblu6os6igg8eiw2",
-    password: "l85bhkrnj3we7h1r",
-    database: "jjrg772djjkv4lyz	"
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "burgers_db"
   });
 }
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
+connection.connect()
 module.exports = connection;
