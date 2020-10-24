f2d9b6aca22dde6cd177f3123ea60b42c775caea
Jennifer Henry <jenhenry1995@gmail.com>
2020-10-23 22:50:19 -0700

    test test

--- config/orm.js
+++ config/orm.js
@@ -77,6 +77,18 @@ var orm = {
         }
     })
   },
+  delete: function(table, condition, cb) {
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
+
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
 };
 
 module.exports = orm;
